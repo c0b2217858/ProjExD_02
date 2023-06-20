ce82b432fd47169160672b38e84e988f01e809fa
c0b2217858 <c0b2217858@edu.teu.ac.jp>
2023-06-20 14:45:23 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,14 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+# 演習3{
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+# }演習3
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +20,12 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+
+# 演習3{
+    # こうかとんSurface（kk_img）からこうかとんRect（kk_rct）を抽出する
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
+# }演習3
 
 # 演習1{
     bomb_img = pg.Surface((20,20))
@@ -23,9 +37,7 @@ def main():
     bomb_rct.center = x,y  # 爆弾の中心座標を乱数で指定する
 # }演習1
 
-# 演習2{
-    vx,vy = +5,+5
-# }演習2
+    vx,vy = +5,+5  # 演習2
 
     clock = pg.time.Clock()
     tmr = 0
@@ -34,12 +46,23 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
+# 演習3{            
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]  # 合計移動量
+        for k, mv in delta.items():
+            if key_lst[k]: 
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
+# }演習3
+
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        
-# 演習2{
-        bomb_rct.move_ip(vx,vy)
-# }演習2
+        #screen.blit(kk_img, [900, 400])
+
+        screen.blit(kk_img, kk_rct)  # 演習3
+
+        bomb_rct.move_ip(vx,vy)  # 演習2
 
 # 演習1{
         #screen.blit(bomb_img, [800, 400])
@@ -48,7 +71,7 @@ def main():
 
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
